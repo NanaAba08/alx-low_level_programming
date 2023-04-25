abea5857a236f7abecf6142f25123a60ea2ee3b0
NanaAba08 <williamsnanaaba@gmail.com>
2023-04-25 14:54:45 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two integers and return the result
+ *
+ * @n: number being added
+ *
+ * @m: number being added
+ *
+ * Return: Always 0.
+ *
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
