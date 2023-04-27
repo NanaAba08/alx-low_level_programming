6c5e7915fc4bb7f6554da814483da522e07ce151
NanaAba08 <williamsnanaaba@gmail.com>
2023-04-27 14:42:24 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * print_line - Draw a straight line
+  * @n: The number of lines to draw
+  * Return: empty
+  */
+void print_line(int n)
+{
+	int a;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < n; a++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
