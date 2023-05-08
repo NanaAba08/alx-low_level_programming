e8dbc0acd1ded7767532b88d181c3aad74c3f137
NanaAba08 <williamsnanaaba@gmail.com>
2023-05-08 13:50:53 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * _memset - Fill memory with constant byte
+  * @s: shows the 1st n byte of memory
+  * @n: the number of byte to fill memory 
+  * @b: constant byte
+  * Return: pointer to memory s
+  */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
